b80bc3ea934cf5a77aa944049835c7d037ceb1f7
raihankhan <raihan@appscode.com>
2022-07-27 12:13:14 +0600

    fixed serviceName not defined error
    Signed-off-by: raihankhan <raihan@appscode.com>

--- Kafka/entrypoint.sh
+++ Kafka/entrypoint.sh
@@ -4,11 +4,11 @@ NODE_ID=${HOSTNAME:6}
 
 
 LISTENERS="SASL_PLAINTEXT://:9092,CONTROLLER://:9093"
-ADVERTISED_LISTENERS="SASL_PLAINTEXT://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092"
+ADVERTISED_LISTENERS="SASL_PLAINTEXT://kafka-$NODE_ID.$SERVICE.$NAMESPACE.svc.cluster.local:9092",
 ENABLED_MECHANISM="PLAIN"
 INTER_BROKER_PROTOCOL="PLAIN"
 INTER_BROKER_SECURITY_PROTOCOL="SASL_PLAINTEXT"
-SECURITY_AUTH="kafka.security.authorizer.AclAuthorizer"
+#SECURITY_AUTH="kafka.security.authorizer.AclAuthorizer"
 
 #if [[ -z "$DISABLE_SECURITY" ]]; then
 #  LISTENERS="SASL_PLAIN://:9092,CONTROLLER://:9093"
@@ -42,7 +42,6 @@ sed -e "s+^node.id=.*+node.id=$NODE_ID+" \
 -e "s+^inter.broker.listener.name=.*+inter.broker.listener.name=$INTER_BROKER_SECURITY_PROTOCOL+" \
 -e "s+^sasl.mechanism.inter.broker.protocol=.*+sasl.mechanism.inter.broker.protocol=$INTER_BROKER_PROTOCOL+" \
 -e "s+^security.inter.broker.protocol=.*+security.inter.broker.protocol=$INTER_BROKER_SECURITY_PROTOCOL+" \
--e "s+^authorizer.class.name=.*+authorizer.class.name=$SECURITY_AUTH+"
 /opt/kafka/config/kraft/server.properties > server.properties.updated \
 && mv server.properties.updated /opt/kafka/config/kraft/server.properties
 
@@ -51,3 +50,4 @@ kafka-storage.sh format -t $CLUSTER_ID -c /opt/kafka/config/kraft/server.propert
 exec kafka-server-start.sh /opt/kafka/config/kraft/server.properties
 
 #-e "s+^listener.name.sasl_ssl.plain.sasl.jaas.config=.*+listener.name.sasl_ssl.plain.sasl.jaas.config=org.apache.kafka.common.security.plain.PlainLoginModule required username="admin" password="admin-password";+" \
+#-e "s+^authorizer.class.name=.*+authorizer.class.name=$SECURITY_AUTH+" \
