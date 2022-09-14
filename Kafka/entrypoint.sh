112937fde8d0b0ffcf5a851023798ce85f5f244e
raihankhan <raihan@appscode.com>
2022-09-14 16:27:02 +0600

    make changes
    Signed-off-by: raihankhan <raihankhanraka@gmail.com>

--- Kafka/entrypoint.sh
+++ Kafka/entrypoint.sh
@@ -3,7 +3,7 @@
 ID=${HOSTNAME##*-}
 NODE=$(echo $HOSTNAME | rev | cut -d- -f1 --complement | rev )
 LISTENERS="PLAINTEXT://:9092,CONTROLLER://:9093"
-ADVERTISED_LISTENERS="PLAINTEXT://$HOSTNAME.$SERVICE.$NAMESPACE.svc.cluster.local:9092"
+ADVERTISED_LISTENERS="PLAINTEXT://$SERVICE.$NAMESPACE.svc.cluster.local:9092"
 
 CONTROLLER_QUORUM_VOTERS=""
 for i in $( seq 0 $REPLICAS); do
@@ -14,22 +14,26 @@ for i in $( seq 0 $REPLICAS); do
     fi
 done
 
-mkdir -p $SHARE_DIR/$ID
 
-if [[ ! -f "$SHARE_DIR/cluster_id" && "$ID" = "0" ]]; then
-    CLUSTER_ID=$(kafka-storage.sh random-uuid)
-    echo $CLUSTER_ID > $SHARE_DIR/cluster_id
+if [[ ! -f "$DATA_DIR/$ID" ]]; then
+  mkdir -p $DATA_DIR/$ID
 else
-    CLUSTER_ID=$(cat $SHARE_DIR/cluster_id)
+  rm -rf $DATA_DIR/$ID/__cluster_metadata-0
 fi
 
-rm -rf $SHARE_DIR/$ID/__cluster_metadata-0
+# create a common cluster_ID for all the nodes.
+if [[ ! -f "$DATA_DIR/cluster_id" && "$ID" = "0" ]]; then
+    CLUSTER_ID=$(kafka-storage.sh random-uuid)
+    echo $CLUSTER_ID > $DATA_DIR/cluster_id
+else
+    CLUSTER_ID=$(cat $DATA_DIR/cluster_id)
+fi
 
 sed -e "s+^node.id=.*+node.id=$ID+" \
 -e "s+^controller.quorum.voters=.*+controller.quorum.voters=$CONTROLLER_QUORUM_VOTERS+" \
 -e "s+^listeners=.*+listeners=$LISTENERS+" \
 -e "s+^advertised.listeners=.*+advertised.listeners=$ADVERTISED_LISTENERS+" \
--e "s+^log.dirs=.*+log.dirs=$SHARE_DIR/$ID+" \
+-e "s+^log.dirs=.*+log.dirs=$DATA_DIR/$ID+" \
 /opt/kafka/config/kraft/server.properties > server.properties.updated \
 && mv server.properties.updated /opt/kafka/config/kraft/server.properties
 
